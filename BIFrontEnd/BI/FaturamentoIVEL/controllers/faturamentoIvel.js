92f7b28dfadf4c695dc48d6b212867982fbb4806
Igor Lessa Robaina de Souza <igorleessa@gmail.com>
2016-04-20 19:38:35 -0300

    pie chart

--- BIFrontEnd/BI/FaturamentoIVEL/controllers/faturamentoIvel.js
+++ BIFrontEnd/BI/FaturamentoIVEL/controllers/faturamentoIvel.js
@@ -6,7 +6,6 @@
 
         AppService.chamarEndpoint('TrazerFaturamento', [],function (retorno) {
             $scope.faturamentos = retorno.data;
-            //$scope.listFaturamentos = retorno.data;
 
             Array.prototype.carregaDados = function (dados) {
                 return this.map(function (obj) {
@@ -14,30 +13,31 @@
                 });
             }
 
-            //$scope.labels = $scope.faturamentos.carregaDados('Operacao');
-            //$scope.data = $scope.faturamentos.carregaDados('ValorNF');
-           
+            var pieData = $scope.faturamentos.carregaDados;
 
+            window.onload = function () {
+                var ctx = document.getElementById("canvas").getContext("2d");
+                window.myPie = new Chart(ctx).Pie(pieData);
+            };
 
-           
-            var barChartData = {
-                labels: $scope.faturamentos.carregaDados('Operacao'),
-                datasets: [
-                    {
-                        fillColor: "rgba(220,220,220,0.5)",
-                        strokeColor: "rgba(220,220,220,0.8)",
-                        highlightFill: "rgba(220,220,220,0.75)",
-                        highlightStroke: "rgba(220,220,220,1)",
-                        data: $scope.faturamentos.carregaDados('ValorNF')
-                    },
-                ]
-            }
+            //var barChartData = {
+            //    labels: $scope.faturamentos.carregaDados('Operacao'),
+            //    datasets: [
+            //        {
+            //            fillColor: "rgba(220,220,220,0.5)",
+            //            strokeColor: "rgba(220,220,220,0.8)",
+            //            highlightFill: "rgba(220,220,220,0.75)",
+            //            highlightStroke: "rgba(220,220,220,1)",
+            //            data: $scope.faturamentos.carregaDados('ValorNF')
+            //        },
+            //    ]
+            //}
 
      
-            var ctx1 = document.getElementById("canvas").getContext("2d");
-            window.myBar = new Chart(ctx1).Bar(barChartData, {
-                responsive: true
-            });
+            //var ctx1 = document.getElementById("canvas").getContext("2d");
+            //window.myBar = new Chart(ctx1).Bar(barChartData, {
+            //    responsive: true
+            //});
             
 
             $scope.atualizando = false;
